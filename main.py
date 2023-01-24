77e24da576af7b6612ab99f62c14d751c8fa7ab7
Michael Parsons <122808160+Voltsunami@users.noreply.github.com>
2023-01-24 22:38:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('My First Streamlit App')
+st.subheader('An Introduction To Asing Streamlit')
+st.write("""
+This is my first web app
+This is going to be FUN!
+""")
